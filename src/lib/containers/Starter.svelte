63428008e4e288af7a3317bce6a0f45e70cd0144
Samuel Aditia <samueladitia95@gmail.com>
2024-06-10 19:21:14 +0700

    feat: add Starter component and intial text

---
+++ src/lib/containers/Starter.svelte
@@ -0,0 +1,32 @@
+<div class="w-screen min-h-screen">
+	<!-- Main Screen -->
+	<!-- Main bg image -->
+	<div class="min-h-screen">
+		<div class="container">
+			<div>
+				<div>TOGETHER WITH OUR PARENTS</div>
+				<div>Mr. Tanuwangsa & Mrs. Jenny Ho</div>
+				<div>Mr. Lie Tjoei Beng & Mrs. Meliana Budiarta</div>
+			</div>
+
+			<div>
+				<div>REYNALDO SAPUTRA</div>
+				<div>&</div>
+				<div>CYNTHIA LIE</div>
+			</div>
+
+			<div>Cordially invite you to the wedding of their children</div>
+
+			<div>VERTICAL LINE LALA</div>
+		</div>
+	</div>
+
+	<!-- Secondary Screen -->
+	<!-- Secondary bg image -->
+	<div class="min-h-screen">
+		<div class="container">
+			<div>I HAVE FOUND THE ONE WHOM MY SOUL LOVES</div>
+			<div>Song of Solomon 3:4</div>
+		</div>
+	</div>
+</div>
