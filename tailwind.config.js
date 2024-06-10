97b81ecaa84618e9432e15c59c24453d6849e1cc
Samuel Aditia <samueladitia95@gmail.com>
2024-06-10 21:34:26 +0700

    feat: add font for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				arizona: ["Arizona", ...defaultTheme.fontFamily.sans],
+				jakarta: ["Jakarta", ...defaultTheme.fontFamily.sans],
+				safira: ["Safira", ...defaultTheme.fontFamily.sans],
+			},
+		},
 		container: {
 			center: true,
 			padding: "2rem",
