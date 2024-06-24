9b1f7447c9ed3d87b261f70ff14267c1e365ea4e
Samuel Aditia <samueladitia95@gmail.com>
2024-06-24 23:34:13 +0700

    feat: add tablet screen for save the date container

--- src/lib/containers/SaveTheDate.svelte
+++ src/lib/containers/SaveTheDate.svelte
@@ -39,23 +39,30 @@
 </script>
 
 <div class="w-screen min-h-screen">
-	<div class="container py-28 text-text-primary text-center flex flex-col gap-12">
-		<div class="font-safira text-4.5xl/tight font-light mx-10">Save The Date</div>
-		<div class="font-jakarta font-light uppercase text-center flex flex-col gap-4">
+	<div class="container py-28 text-text-primary text-center flex flex-col gap-12 md:gap-16">
+		<div class="font-safira uppercase text-4.5xl/tight md:text-5xl font-light mx-10">
+			Save The Date
+		</div>
+		<div class="font-jakarta font-light uppercase text-center flex flex-col gap-4 md:hidden">
 			<div>{weddingDay.format("dddd")}</div>
 			<div>{weddingDay.format("MMMM D YYYY")}</div>
+		</div>
+
+		<div class="font-jakarta text-2xl font-light uppercase text-center hidden md:block md:-mt-12">
+			<div>{weddingDay.format("dddd, MMMM D YYYY")}</div>
 		</div>
 
 		<div class="grid grid-cols-2 gap-12">
 			{#each countdowns as countdown}
 				<div class="flex flex-col justify-center items-center gap-4">
-					<div class=" text-3xl md:text-5xl font-arizona font-light">{countdown.value}</div>
+					<div class=" text-3xl md:text-6xl font-arizona font-light">{countdown.value}</div>
 					<div class="text-sm md:text-2xl font-jakarta font-light">{countdown.type}(S)</div>
 				</div>
 			{/each}
 		</div>
 		<div>
-			<button class="px-6 py-2.5 bg-bg-primary text-white rounded-full font-jakarta"
+			<button
+				class="px-6 md:px-9 py-2.5 md:py-4 bg-bg-primary text-white rounded-full font-jakarta md:text-2xl"
 				>Add to Calender</button
 			>
 		</div>
