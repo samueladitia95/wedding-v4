294a36df0a40c8b96c239b0ae6cf9551aeed04e8
Samuel Aditia <samueladitia95@gmail.com>
2024-06-26 01:01:24 +0700

    feat: add animation for save the date

--- src/lib/containers/SaveTheDate.svelte
+++ src/lib/containers/SaveTheDate.svelte
@@ -38,51 +38,82 @@
 	};
 </script>
 
-<div class="w-screen min-h-screen">
+<div
+	class="w-screen min-h-screen"
+	use:inview={{
+		rootMargin: "-200px",
+		unobserveOnEnter: true,
+	}}
+	on:inview_change={handleChange}
+>
 	<div class="container py-28 text-text-primary text-center flex flex-col gap-12 md:gap-16">
-		<div class="font-safira uppercase text-4.5xl/tight md:text-5xl font-light mx-10">
-			Save The Date
-		</div>
-		<div class="font-jakarta font-light uppercase text-center flex flex-col gap-4 md:hidden">
-			<div>{weddingDay.format("dddd")}</div>
-			<div>{weddingDay.format("MMMM D YYYY")}</div>
-		</div>
+		{#if isShow}
+			<div
+				class="font-safira uppercase text-4.5xl/tight md:text-5xl font-light mx-10"
+				in:fade={{ duration: 1000, delay: 500 }}
+			>
+				Save The Date
+			</div>
+		{/if}
+		{#if isShow}
+			<div
+				class="font-jakarta font-light uppercase text-center flex flex-col gap-4 md:hidden"
+				in:fade={{ duration: 1000, delay: 500 }}
+			>
+				<div>{weddingDay.format("dddd")}</div>
+				<div>{weddingDay.format("MMMM D YYYY")}</div>
+			</div>
 
-		<div class="font-jakarta text-2xl font-light uppercase text-center hidden md:block md:-mt-12">
-			<div>{weddingDay.format("dddd, MMMM D YYYY")}</div>
-		</div>
+			<div
+				class="font-jakarta text-2xl font-light uppercase text-center hidden md:block md:-mt-12"
+				in:fade={{ duration: 1000, delay: 500 }}
+			>
+				<div>{weddingDay.format("dddd, MMMM D YYYY")}</div>
+			</div>
+		{/if}
 
 		<div class="grid grid-cols-2 lg:grid-cols-4 gap-12">
-			{#each countdowns as countdown}
-				<div class="flex flex-col justify-center items-center gap-4">
-					<div class=" text-3xl md:text-6xl font-arizona font-light">{countdown.value}</div>
-					<div class="text-sm md:text-2xl font-jakarta font-light">{countdown.type}(S)</div>
-				</div>
+			{#each countdowns as countdown, index}
+				{#if isShow}
+					<div
+						class="flex flex-col justify-center items-center gap-4"
+						transition:fly={{ x: -200, duration: 1000, delay: 1000 + 200 * (index + 1) }}
+					>
+						<div class=" text-3xl md:text-6xl font-arizona font-light">{countdown.value}</div>
+						<div class="text-sm md:text-2xl font-jakarta font-light">{countdown.type}(S)</div>
+					</div>
+				{/if}
 			{/each}
 		</div>
-		<div>
-			<button
-				class="px-6 md:px-9 py-2.5 md:py-4 bg-bg-primary text-white rounded-full font-jakarta md:text-2xl"
-				>Add to Calender</button
-			>
-		</div>
+		{#if isShow}
+			<div>
+				<button
+					class="px-6 md:px-9 py-2.5 md:py-4 bg-bg-primary text-white rounded-full font-jakarta md:text-2xl"
+					transition:fly={{ x: -200, duration: 2000, delay: 1500 }}
+				>
+					Add to Calender
+				</button>
+			</div>
+		{/if}
 
-		<div class="">
-			<swiper-container
-				space-between="8"
-				loop="true"
-				free-mode="true"
-				speed="5000"
-				effect="slide"
-				autoplay="true"
-			>
-				{#each carauselImages as image}
-					<swiper-slide>
-						<img src={image} alt="invite" class="rounded" />
-					</swiper-slide>
-				{/each}
-			</swiper-container>
-		</div>
+		{#if isShow}
+			<div class="transition:fly={{ y: -200, duration: 2000, delay: 1500 }}">
+				<swiper-container
+					space-between="8"
+					loop="true"
+					free-mode="true"
+					speed="5000"
+					effect="slide"
+					autoplay="true"
+				>
+					{#each carauselImages as image}
+						<swiper-slide>
+							<img src={image} alt="invite" class="rounded" />
+						</swiper-slide>
+					{/each}
+				</swiper-container>
+			</div>
+		{/if}
 	</div>
 </div>
 
