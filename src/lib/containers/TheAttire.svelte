f4f0d6d44de6d335ae9f8272637fc661fb5cdf81
Samuel Aditia <samueladitia95@gmail.com>
2024-06-26 01:10:57 +0700

    feat: add animation for the attire

--- src/lib/containers/TheAttire.svelte
+++ src/lib/containers/TheAttire.svelte
@@ -1,19 +1,59 @@
-<div class="bg-bg-attire h-screen w-screen">
+<script lang="ts">
+	import { inview } from "svelte-inview";
+	import { fly } from "svelte/transition";
+
+	let isShow: boolean = false;
+
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>): void => {
+		if (!isShow && detail.inView) isShow = true;
+	};
+</script>
+
+<div
+	class="bg-bg-attire h-screen w-screen"
+	use:inview={{
+		rootMargin: "-200px",
+		unobserveOnEnter: true,
+	}}
+	on:inview_change={handleChange}
+>
 	<div class="container h-full w-full flex flex-col justify-center items-center">
 		<div class="text-white text-center flex flex-col gap-6 md:gap-16 justify-center items-center">
-			<div class="font-safira font-light text-5xl lg:text-7xl">THE ATTIRE</div>
-			<div class="font-arizona font-light text-base/relaxed md:text-xl/relaxed md:max-w-96">
-				We kindly encourage our guests to wear these colors for our special day
-			</div>
-			<div class="py-6 md:py-0 flex flex-col gap-4">
-				<div class="font-jakarta uppercase md:text-2xl">Holy Matrimony</div>
-				<div class="font-arizona font-light md:text-2xl">White Semi Formal Attire</div>
-			</div>
+			{#if isShow}
+				<div
+					class="font-safira font-light text-5xl lg:text-7xl"
+					transition:fly={{ x: -200, duration: 1000, delay: 500 }}
+				>
+					THE ATTIRE
+				</div>
+			{/if}
+			{#if isShow}
+				<div
+					class="font-arizona font-light text-base/relaxed md:text-xl/relaxed md:max-w-96"
+					transition:fly={{ x: -200, duration: 1000, delay: 1000 }}
+				>
+					We kindly encourage our guests to wear these colors for our special day
+				</div>
+			{/if}
+			{#if isShow}
+				<div
+					class="py-6 md:py-0 flex flex-col gap-4"
+					transition:fly={{ x: -200, duration: 1000, delay: 1500 }}
+				>
+					<div class="font-jakarta uppercase md:text-2xl">Holy Matrimony</div>
+					<div class="font-arizona font-light md:text-2xl">White Semi Formal Attire</div>
+				</div>
+			{/if}
 
-			<div class="py-6 md:py-0 flex flex-col gap-4">
-				<div class="font-jakarta uppercase md:text-2xl">Reception</div>
-				<div class="font-arizona font-light md:text-2xl">Formal Attire and Dark Colors</div>
-			</div>
+			{#if isShow}
+				<div
+					class="py-6 md:py-0 flex flex-col gap-4"
+					transition:fly={{ x: -200, duration: 1000, delay: 2000 }}
+				>
+					<div class="font-jakarta uppercase md:text-2xl">Reception</div>
+					<div class="font-arizona font-light md:text-2xl">Formal Attire and Dark Colors</div>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
