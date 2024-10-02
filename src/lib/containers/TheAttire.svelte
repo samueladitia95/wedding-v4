27bcef27635f7de3b719483fe787d3a8b4cef4bd
rezafiansyah <rezafiansyah.putra@gmail.com>
2024-10-02 17:06:51 +0700

    adding underline in the attire

--- src/lib/containers/TheAttire.svelte
+++ src/lib/containers/TheAttire.svelte
@@ -42,7 +42,7 @@
 				>
 					<!-- <div class="font-jakarta uppercase md:text-2xl">Holy Matrimony</div> -->
 					<!-- <div class="font-arizona font-light md:text-2xl">White Semi Formal Attire</div>-->
-					<div class="font-arizona font-light md:text-2xl">White Formal Attire</div>
+					<div class="font-arizona font-light underline md:text-2xl">White Formal Attire</div>
 				</div>
 			{/if}
 
