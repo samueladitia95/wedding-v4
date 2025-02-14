f48f99219a371e464756974957f38a988da6e1a8
rezafiansyah <rezafiansyah.putra@gmail.com>
2025-02-14 14:35:50 +0700

    fix: small details for capital letter and full stop

--- src/lib/containers/QnA.svelte
+++ src/lib/containers/QnA.svelte
@@ -40,7 +40,7 @@
 		{
 			question: "CAN WE TAKE PICTURES DURING THE HOLY MATRIMONY?",
 			answer:
-				"Please be mindful during the ceremony we would love it to be an unplugged ceremony so guests can truly immerse themselves & be present. After the ceremony please feel free to take pictures.",
+				"Please be mindful during the ceremony. We would love it to be an unplugged ceremony so guests can truly immerse themselves & be present. After the ceremony please feel free to take pictures.",
 		},
 	];
 
